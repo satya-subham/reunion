054a60ae3c78c6252c5487ad2f6b44b573a1b398
satya-subham <satyasubham85@gmail.com>
2022-12-02 16:38:15 +0530

    reunion

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import { Favorite } from './components/favourite/Favorite';
-import { Header } from './components/header/Header';
-import { Main } from './components/main/Main';
+import { Favorite } from "./components/favourite/Favorite";
+import { Header } from "./components/header/Header";
+import { Main } from "./components/main/Main";
 
-import './App.css';
-import 'antd/dist/reset.css';
+import "./App.css";
+import "antd/dist/reset.css";
 
 function App() {
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
   return (
     <>
-      <Header setSearch={setSearch}/>
+      <Header setSearch={setSearch} />
       <Routes>
-      <Route path='/' element={<Main search={search}/>}/>
-        <Route path='/Favorite' element={<Favorite/>}/>
+        <Route path="/" element={<Main search={search} />} />
+        <Route path="/Favorite" element={<Favorite />} />
       </Routes>
     </>
   );
